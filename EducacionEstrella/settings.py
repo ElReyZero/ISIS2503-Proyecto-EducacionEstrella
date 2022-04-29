677be0b2d84427088712de233255e89252d35b67
Juan Romero <juan.romero1201@gmail.com>
2022-04-29 14:52:26 -0500

    changed to https

--- EducacionEstrella/settings.py
+++ EducacionEstrella/settings.py
@@ -129,7 +129,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = "/login/auth0" 
 LOGIN_REDIRECT_URL = "/" 
 #TODO Cambiar la ip
-LOGOUT_REDIRECT_URL = "https://isis2503-elreyzero.auth0.com/v2/logout?returnTo=http%3A%2F%2Fip_publica_instancia:8000" 
+LOGOUT_REDIRECT_URL = "https://isis2503-elreyzero.auth0.com/v2/logout?returnTo=https%3A%2F%2Fip_publica_instancia" 
 SOCIAL_AUTH_TRAILING_SLASH = False # Remove end slash from routes 
 SOCIAL_AUTH_AUTH0_DOMAIN = 'isis2503-elreyzero.us.auth0.com' 
 SOCIAL_AUTH_AUTH0_KEY = 'iGKClII0copmrNWRZNMvXJeO2NQUvP8w' 
