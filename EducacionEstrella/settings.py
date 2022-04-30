75f7d8c8121aa7f74a511b4d80eb590156d135e7
Juan Romero <juan.romero1201@gmail.com>
2022-04-30 15:10:28 -0500

    Test

--- EducacionEstrella/settings.py
+++ EducacionEstrella/settings.py
@@ -146,7 +146,6 @@ ip = "3.91.194.82"
 LOGIN_URL = "/login/auth0" 
 LOGIN_REDIRECT_URL = "/"
 SOCIAL_AUTH_LOGIN_REDIRECT_URL = 'https://'+ip
-SOCIAL_AUTH_SANITIZE_REDIRECTS = False
 LOGOUT_REDIRECT_URL = "https://isis2503-elreyzero.us.auth0.com/v2/logout?returnTo=https%3A%2F%2F"+ip 
 SOCIAL_AUTH_TRAILING_SLASH = False # Remove end slash from routes 
 SOCIAL_AUTH_AUTH0_DOMAIN = 'isis2503-elreyzero.us.auth0.com' 
