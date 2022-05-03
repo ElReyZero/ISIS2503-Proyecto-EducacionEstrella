e04bbe0c6e26c6f0a77fd30fd14531c51442783f
ElReyZero <31524106+ElReyZero@users.noreply.github.com>
2022-05-03 12:37:56 -0500

    Update hash.js

--- EducacionEstrella/static/hash.js
+++ EducacionEstrella/static/hash.js
@@ -4,3 +4,5 @@ function setHash()
     var string = document.getElementById("estudiante").value + document.getElementById("analista").value + String(document.getElementById("montoAPagar").value) + document.getElementById("fechaSolicitud").value + document.getElementById("fechaAprobacion").value;
     document.getElementById("hash").value = createHash('sha256').update(string).digest('hex');
 };
+
+document.getElementById("submitButton").addEventListener("click", setHash());
