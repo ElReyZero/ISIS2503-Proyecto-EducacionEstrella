d481e5c50a278f403f6d7e9346e0518c89ed4874
ElReyZero <juan.romero1201@gmail.com>
2022-05-09 11:11:24 -0500

    Fixing url

--- ModuloFinanciero/urls.py
+++ ModuloFinanciero/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.dashboard_view, name='dashboard'),
     path('solicitud/create', views.solicitud_create, name='solicitudCreate'),
-    path('solicitud/create_exempt', views.solicitud_create_exempt, name='solicitudCreateExempt'),
+    path('solicitud/create_exempt', views.solicitud_create_csrf_exempt, name='solicitudCreateExempt'),
     path('solicitud/<id>', views.solicitud_view, name='solicitud'),
 ]
