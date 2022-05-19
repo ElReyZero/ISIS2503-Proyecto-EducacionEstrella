530ab23a0f2417272e353db94ac23436784385e1
ElReyZero <juan.romero1201@gmail.com>
2022-05-19 10:17:10 -0500

    Migrations

--- ModuloFinanciero/admin.py
+++ ModuloFinanciero/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from ModuloFinanciero.models import EstudianteEstrella, SolicitudCredito
 # Register your models here.
+
+admin.site.register(EstudianteEstrella)
+admin.site.register(SolicitudCredito)
