911cea69760947404345a58a78e60aedb549387f
jmarinm <j.marinm@uniandes.edu.co>
2022-05-23 10:48:17 -0500

    Holi

--- ReportGatherer/sentences.sql
+++ ReportGatherer/sentences.sql
@@ -3,4 +3,10 @@ tasa de creditos = aceptados/solicitados
 total recaudo semestral 
 total cuotas en mora
 
-SELECT
+SELECTSELECT a.nombre, SUM(a.monto)
+FROM
+    (SELECT ee.nombre as nombre, CASE WHEN s.pagado = 1 THEN s.montoAPagar ELSE 0 as monto
+    FROM public."ModuloFinanciero_solicitudcredito" s, public."ModuloFinanciero_estudianteestrella" ee
+    WHERE s.estudiante_id = ee.id
+    GROUP BY s.estudiante_id, s.carrera) a
+GROUP BY a.nombre
