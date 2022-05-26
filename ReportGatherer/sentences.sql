5b8009df2388a7aaf06e645b0a30f35cd39b5f03
Lauraquiroga <60222757+Lauraquiroga@users.noreply.github.com>
2022-05-26 17:47:47 -0500

    Sentencia tasa de aprobación

--- ReportGatherer/sentences.sql
+++ ReportGatherer/sentences.sql
@@ -3,10 +3,37 @@ tasa de creditos = aceptados/solicitados
 total recaudo semestral 
 total cuotas en mora
 
-SELECTSELECT a.nombre, SUM(a.monto)
+SELECT a.nombre, SUM(a.monto)
 FROM
-    (SELECT ee.nombre as nombre, CASE WHEN s.pagado = 1 THEN s.montoAPagar ELSE 0 as monto
+    (SELECT ee.nombre as nombre, CASE WHEN s.pagado = 1 THEN s.montoAPagar ELSE 0 END as monto
     FROM public."ModuloFinanciero_solicitudcredito" s, public."ModuloFinanciero_estudianteestrella" ee
     WHERE s.estudiante_id = ee.id
     GROUP BY s.estudiante_id, s.carrera) a
 GROUP BY a.nombre
+
+--Calcular total creditos aprobados
+SELECT COUNT(*), creditos_aprobados
+FROM public."ModuloFinanciero_solicitudcredito" s
+WHERE s.fechaAprobacion IS NOT NULL
+
+SELECT COUNT(*) creditos_aprobados, ee.-criterio-
+FROM public."ModuloFinanciero_solicitudcredito" s, public."ModuloFinanciero_estudianteestrella" ee
+WHERE s.estudiante_id = ee.id AND s.fechaAprobacion IS NOT NULL
+GROUP BY ee.-criterio-
+
+SELECT b.-criterio-, 
+    CASE WHEN a.creditos_aprobados IS NOT NULL THEN ROUND((a.creditos_aprobados/b.creditos_totales),2) 
+    ELSE 0 END AS tasa_aprobacion
+FROM (
+    SELECT CAST (COUNT(*) as numeric(9,2))creditos_aprobados, ee.-criterio-
+    FROM public."ModuloFinanciero_solicitudcredito" s, public."ModuloFinanciero_estudianteestrella" ee
+    WHERE s.estudiante_id = ee.id AND s."fechaAprobacion" IS NOT NULL
+    GROUP BY ee.-criterio-
+    ) a RIGHT JOIN
+    (
+    SELECT CAST (COUNT(*) as numeric(9,2)) creditos_totales, ee.-criterio-
+    FROM public."ModuloFinanciero_solicitudcredito" s,public."ModuloFinanciero_estudianteestrella" ee
+    WHERE s.estudiante_id = ee.id    
+    GROUP BY ee.-criterio-
+    ) b
+ON a.-criterio- = b.-criterio-;
